7d829f6915ef49a029ce3652c3601854fe80c050
TaHuC <TaHuC@hotmail.com>
2017-02-03 23:02:55 +0200

    Finish

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReverseAnArrayOfIntegers
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			var getNum = int.Parse(Console.ReadLine());
+
+			var createArray = new int[getNum];
+
+			for (var i = 0; i < getNum; i++)
+			{
+				createArray[getNum - i - 1] = int.Parse(Console.ReadLine());
+			}
+
+			for (var i = 0; i < getNum; i++)
+			{
+				if (i == getNum - 1)
+				{
+					Console.Write(createArray[i]);
+				}
+				else
+				{
+					Console.Write($"{createArray[i]} ");
+				}
+			}
+		}
+	}
+}
